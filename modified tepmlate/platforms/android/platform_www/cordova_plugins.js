a8cf67255e5f3100cba1e71510b64836669e01f4
Eddie Alshehri <aalshehri@luc.edu>
2016-12-02 05:16:14 -0600

    upload files

---
+++ modified tepmlate/platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-sqlite-storage": "2.0.0"
+};
+// BOTTOM OF METADATA
+});
